aa6a07966d418f7e90442e3845d61985ed0ff0ac
WumIngAcgS <15665495278@163.com>
2025-05-18 17:31:12 +0800

    1

---
+++ C#/VirtualOverride4_16/VirtualOverride4_16/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverride4_16
+{
+    internal class Animal
+    {
+        public virtual void Move()
+        {
+            Console.WriteLine("Animal speaks");
+            System.Windows.Forms.MessageBox.Show("所有动物都有的走动行为");
+        }
+
+    }
+}
---
+++ C#/VirtualOverride4_16/VirtualOverride4_16/Fish.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverride4_16
+{
+    internal class Fish : Animal
+    {
+        public override void Move()
+        {
+            base.Move();
+            Console.WriteLine("Fish swims");
+            System.Windows.Forms.MessageBox.Show("Swim   鱼在水中游动");
+        }
+    }
+    
+    
+}
